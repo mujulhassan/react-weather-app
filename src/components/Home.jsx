963da945af472ba6ab58c3328d55b571f0d89f88
mujulhassan <mujulhassan@gmail.com>
2023-03-14 20:21:59 +0000

    Final commits

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,36 +1,58 @@
-import { useState } from "react";
-  import WeatherForecast from "./WeatherForecast";
+// src/components/Home
   
-  export default function Home() {
-    const [searchTerm, setSearchTerm] = useState("");
-  
-    const getWeatherInfo = async () => {
-      try {
-        let url = `https://api.openweathermap.org/data/2.5/weather?q=${searchTerm}&appid=${process.env.REACT_APP_OPEN_WEATHER_KEY}`;
-  
-        let res = await fetch(url);
-        let data = await res.json();
-        console.log(data);
-      } catch (error) {
-        console.log(error);
-      }
-    };
-  
-    return (
-      <>
-        <form role="search">
-          <input
-            type="search"
-            aria-label="Search for a city"
-            placeholder="London"
-            value={searchTerm}
-            onChange={(event) => setSearchTerm(event.target.value)}
-          />
-          <button onClick={() => getWeatherInfo()} type="button">
-            Search
-          </button>
-        </form>
-        <WeatherForecast />
-      </>
-    );
-  }
+import { useState, useEffect } from "react";
+import WeatherForecast from "./WeatherForecast";
+
+export default function Home() {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [weatherInfo, setWeatherInfo] = useState(null);
+
+
+  const getWeatherInfo = async () => {
+    try {
+      let url = `https://api.openweathermap.org/data/2.5/weather?q=${searchTerm}&appid=${process.env.REACT_APP_OPEN_WEATHER_KEY}&units=metric`;
+
+      let res = await fetch(url);
+      let data = await res.json();
+
+      const { main, name, sys, weather, wind } = data;
+
+      const newWeatherInfo = {
+        humidity: main.humidity,
+        location: `${name}, ${sys.country}`,
+        pressure: main.pressure,
+        sunset: sys.sunset,
+        temperature: main.temp,
+        weatherIcon: weather[0].icon,
+        weatherType: weather[0].main,
+        windSpeed: wind.speed,
+      };
+
+      setWeatherInfo(newWeatherInfo);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getWeatherInfo()
+  },[searchTerm])
+
+  return (
+    <>
+      <form role="search">
+        <input
+          type="search"
+          aria-label="Search for a city"
+          placeholder="London"
+          value={searchTerm}
+          onChange={(event) => setSearchTerm(event.target.value)}
+        />
+        <button onClick={() => getWeatherInfo()} type="button">
+          Search
+        </button>
+      </form>
+      {weatherInfo && <WeatherForecast {...weatherInfo} />}
+    </>
+  );
+}
--- src/components/WeatherForecast.jsx
+++ src/components/WeatherForecast.jsx
@@ -1,41 +1,80 @@
-import weatherIcon from "../assets/openweathericons/04d.svg";
-  
-  export default function WeatherForecast() {
-    return (
-      <article className="forecast">
-        <div className="main">
-          <div className="info">
-            <p className="location">London, UK</p>
-            <time dateTime="2023-02-08" className="date">
-              Wed, 8 February
-            </time>
-            <p className="conditions">Clouds</p>
-          </div>
-          <div className="temperature">
-            <img src={weatherIcon} alt="" />
-            <p>2°</p>
-          </div>
+function getDate(date) {
+  const dateOptions = {
+    weekday: "short",
+    month: "long",
+    day: "numeric",
+  };
+
+  let readable = date.toLocaleDateString(undefined, dateOptions);
+  let dateTime = date.toISOString().split("T")[0];
+
+  return { readable, dateTime };
+}
+
+function getTime(date) {
+  const timeOptions = {
+    hour: "2-digit",
+    minute: "2-digit",
+    dayPeriod: "short",
+  };
+
+  let readable = date.toLocaleTimeString(undefined, timeOptions);
+  let dateTime = date.getHours() + ":" + date.getMinutes();
+
+  return { readable, dateTime };
+}
+
+export default function WeatherForecast({
+  humidity,
+  location,
+  pressure,
+  sunset,
+  temperature,
+  weatherIcon,
+  weatherType,
+  windSpeed,
+}) {
+  return (
+    <article className="forecast">
+      <div className="main">
+        <div className="info">
+          <p className="location">{location}</p>
+          <time dateTime={getDate(new Date()).dateTime} className="date">
+            {getDate(new Date()).readable}
+          </time>
+          <p className="conditions">{weatherType}</p>
         </div>
-        <dl>
-          <div className="additional-info">
-            <dt>Sunset</dt>
-            <dd>
-              <time dateTime="22:31">10:31PM</time>
-            </dd>
-          </div>
-          <div className="additional-info">
-            <dt>Humidity</dt>
-            <dd>85%</dd>
-          </div>
-          <div className="additional-info">
-            <dt>Pressure</dt>
-            <dd>1031</dd>
-          </div>
-          <div className="additional-info">
-            <dt>Wind speed</dt>
-            <dd>2.57km/h</dd>
-          </div>
-        </dl>
-      </article>
-    );
-  }
+        <div className="temperature">
+          <img
+            src={require(`../assets/openweathericons/${weatherIcon}.svg`)}
+            alt=""
+          />
+          <p>{Math.round(temperature)}°</p>
+        </div>
+      </div>
+      <dl>
+        <div className="additional-info">
+          <dt>Sunset</dt>
+          <dd>
+            <time dateTime={getTime(new Date(sunset)).dateTime}>
+              {getTime(new Date(sunset)).readable}
+            </time>
+          </dd>
+        </div>
+
+        <div className="additional-info">
+          <dt>Humidity</dt>
+          <dd>{humidity}%</dd>
+        </div>
+        <div className="additional-info">
+          <dt>Pressure</dt>
+          <dd>{pressure}</dd>
+        </div>
+        <div className="additional-info">
+          <dt>Wind speed</dt>
+          <dd>{windSpeed}km/h</dd>
+        </div>
+      </dl>
+    </article>
+  );
+}
